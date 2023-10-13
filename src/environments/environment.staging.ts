76861f1dbea497e82b7a357cdd9993c58fe847e7
maksim <maksim@novastar.su>
2023-10-13 14:50:58 +0400

    Added stage env & updated deployment action

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,11 @@
+import { Environment } from './environment.type';
+
+export const environment: Environment = {
+  production: true,
+  auth0: {
+    domain: 'dev-sv3qrahltc0ebh7y.us.auth0.com',
+    clientId: 'wbhoXTThDdpAdqNV1WlGHwEJdvKdoMcB',
+    audience: 'https://dev-sv3qrahltc0ebh7y.us.auth0.com/api/v2/',
+  },
+  apiUrl: 'https://api-dev.g-track.eu',
+};
