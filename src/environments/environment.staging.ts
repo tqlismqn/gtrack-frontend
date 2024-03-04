203cc141edac2d825501c5f98cf1262073b9c63b
maksim <maksim@novastar.su>
2024-03-04 16:08:41 +0400

    Апдейт auth0 domain

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -3,7 +3,7 @@ import { Environment } from './environment.type';
 export const environment: Environment = {
   production: true,
   auth0: {
-    domain: 'dev-sv3qrahltc0ebh7y.us.auth0.com',
+    domain: 'login.g-track.eu',
     clientId: 'wbhoXTThDdpAdqNV1WlGHwEJdvKdoMcB',
     audience: 'https://dev-sv3qrahltc0ebh7y.us.auth0.com/api/v2/',
   },
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@ import { Environment } from './environment.type';
 export const environment: Environment = {
   production: true,
   auth0: {
-    domain: 'dev-sv3qrahltc0ebh7y.us.auth0.com',
+    domain: 'login.g-track.eu',
     clientId: 'x7qSOyrqJIHx4oytlzXg21WMlCESuPRd',
     audience: 'https://dev-sv3qrahltc0ebh7y.us.auth0.com/api/v2/',
   },
